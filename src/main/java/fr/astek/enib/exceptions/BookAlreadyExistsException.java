9eb500127fa47ef343db9d502851b40c6f7e033d
killian <kurun02@gmail.com>
2025-06-10 11:57:59 +0200

    feat: ajout javadoc

--- src/main/java/fr/astek/enib/exceptions/BookAlreadyExistsException.java
+++ src/main/java/fr/astek/enib/exceptions/BookAlreadyExistsException.java
@@ -2,6 +2,13 @@ package fr.astek.enib.exceptions;
 
 import org.springframework.http.HttpStatus;
 
+
+/**
+ * Exception levée lorsque l'on tente d'ajouter un livre qui existe déjà dans la base de données.
+ * <p>
+ * Cette exception hérite de {@link BookException} et retourne le statut HTTP 409 CONFLICT.
+ * </p>
+ */
 public class BookAlreadyExistsException extends BookException {
     @Override
     public HttpStatus getStatus() {
